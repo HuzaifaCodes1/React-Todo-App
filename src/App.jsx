3614f0d6280cfb0ee832fbfca58df7892cc036e8
HuzaifaCodes1 <kkhuzaifa2004@gmail.com>
2024-10-01 08:34:01 -0700

    new code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-function App(){
-  return(
-    <h1>HELLO WORLD</h1>
-  )
-}
-export default App
+// src/Todo.js
+import React from 'react';
+
+const Todo = ({ todo, onDelete }) => {
+  return (
+    <div>
+      <span>{todo.text}</span>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default Todo;
