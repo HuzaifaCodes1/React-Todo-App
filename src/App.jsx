9ba7fee583cae6c4eba15e75f4c9082d199b161a
HuzaifaCodes1 <kkhuzaifa2004@gmail.com>
2024-10-01 15:25:07 -0700

    updated css and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import './App.css'; // Make sure to import your Checkbox styles if needed
 
 function App() {
@@ -55,14 +54,20 @@ function App() {
               />
               <label htmlFor={`check-${index}`} className="label">
                 <svg width="45" height="45" viewBox="0 0 95 95">
-                  <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"></rect>
+                  <rect
+                    x="30"
+                    y="20"
+                    width="50"
+                    height="50"
+                    className={`checkbox-rect ${todo.completed ? 'checked' : ''}`}
+                  />
                   <g transform="translate(0,-952.36222)">
                     <path
                       d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4"
                       stroke="black"
                       strokeWidth="3"
                       fill="none"
-                    ></path>
+                    />
                   </g>
                 </svg>
                 <span>{todo.text}</span>
@@ -77,4 +82,3 @@ function App() {
 }
 
 export default App;
-
